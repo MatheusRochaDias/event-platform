397fb8d5acadcc4780ced113abe6319320eca5d4
MatheusRochaDias <matheusrochadias@hotmail.com>
2022-06-23 01:59:03 -0300

    Commit segunda aula

---
+++ event-platform/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { Logo } from "./Logo";
+
+export function Header() {
+    return (
+        <header className="w-full flex items-center justify-center bg-gray-700 border-b border-gray-600 py-3">
+            <Logo />
+        </header>
+    )
+}
