bc8097680eb83142b5ee42ea339ab98f275c54a4
MatheusRochaDias <matheusrochadias@hotmail.com>
2022-06-26 03:32:09 -0300

    Aula 4

--- event-platform/src/pages/Event.tsx
+++ event-platform/src/pages/Event.tsx
@@ -5,10 +5,12 @@ import { Video } from "../components/Video";
 
 export function Event() {
     const { slug } = useParams<{ slug: string }>()
+    
 
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
+            
             <main className="flex flex-1">
                 {slug  
                 ? <Video lessonSlug={slug}/> 
